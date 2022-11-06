9a9f5f0399637c04f5d9bcd86af580b2095fb570
Ca-Jaye Clarke <cajayeclarke44@gmail.come>
2022-11-05 22:43:09 -0500

    Removed node modules and env from git history

--- models/auth.js
+++ models/auth.js
@@ -4,7 +4,7 @@ const { model, Schema } = require("mongoose")
 const User = new Schema({
     username: { type: String, required: [true, "Username is required"], unique: [true, "Username already exists"] },
     password: { type: String, required: [true, "Password is required"] },
-    buckets: [{ type: Schema.type.ObjectId, ref: "Bucket" }] //add user._id when you create bucket
+    buckets: [{ type: Schema.Types.ObjectId, ref: "Bucket" }] //add user._id when you create bucket
 })
 
 User.pre('save', async function () {
